d7501bd8019685147ae299af1f52034dc470746d
easytourbrasil <easytourbrasil.rn@gmail.com>
2018-01-11 17:50:35 -0300

    Update

--- conf.bash
+++ conf.bash
@@ -6,7 +6,3 @@ sudo chmod 777 -R easytourWeb/storage
 sudo chmod 777 -R easytourWeb/bootstrap/cache
 
 sudo docker-compose up -d
-
-sh -c 'sudo docker exec -it easytourapi bash'
-php artisan migrate
-'exit'
