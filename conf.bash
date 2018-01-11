cefd46e4b421d2f04d3166b234ed0d82ceae1e6f
easytourbrasil <easytourbrasil.rn@gmail.com>
2018-01-11 17:08:38 -0300

    Update

--- conf.bash
+++ conf.bash
@@ -1,3 +1,5 @@
 #!/bin/bash
 sudo chmod 777 -R easytourApi/storage
+sudo chmod 777 -R easytourApi/bootstrap/cache
 sudo chmod 777 -R easytourWeb/storage
+sudo chmod 777 -R easytourWeb/bootstrap/cache
