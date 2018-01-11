b06a44fe615fdde3461745a4182ed67889e3231e
easytourbrasil <easytourbrasil.rn@gmail.com>
2018-01-11 17:30:35 -0300

    Update

--- conf.bash
+++ conf.bash
@@ -7,6 +7,6 @@ sudo chmod 777 -R easytourWeb/bootstrap/cache
 
 sudo docker-compose up -d
 
-sudo docker 'exec' -it easytourapi bash #!/usr/bin/env bash
+sudo docker $( printf 'exec' ) -it easytourapi bash #!/usr/bin/env bash
 php artisan migrate
 'exit'
