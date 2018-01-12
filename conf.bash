c2f5e235a0e97cd38c719027767f176d510e1a8b
easytourbrasil <easytourbrasil.rn@gmail.com>
2018-01-11 22:21:30 -0300

    Update

--- conf.bash
+++ conf.bash
@@ -7,5 +7,6 @@ sudo chmod 777 -R easytourWeb/bootstrap/cache
 
 sudo docker-compose up -d
 
+sleep 10
 idContainer=$(sudo docker ps -aqf "name=easytourapi")
 sudo docker exec $idContainer php artisan migrate
